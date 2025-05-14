d24e29ff691cd9df1a057539eb6f44aff8eafe9b
Lillie Rugtveit <126776478+LillieEntur@users.noreply.github.com>
2025-05-14 09:26:44 +0200

    Extract out JSDoc typedefs to own file

--- scanner-action/index.js
+++ scanner-action/index.js
@@ -6,54 +6,7 @@ import { Octokit } from "octokit";
 import { throttling } from "@octokit/plugin-throttling";
 import { Ajv } from 'ajv'
 
-
-
-/* JSDoc type definitions */
-/** 
- * @typedef ScannerMetadata
- * @property {string} id - unique identifier
- * @property {string} name - human redable name
- * @property {string} owner - responsible team or developer
- */
-
-/** 
- * @typedef AllowlistEntry
- * @property {(string|null)} cve - cve-id
- * @property {(string|null)} cwe - cwe-id
- * @property {string} comment - comment explaining why the vulnerability is dismissed
- * @property {("false_positive"|"wont_fix"|"test")} reason - reason for dismissing the vulnerability
- */
-
-/**
- * @typedef ScannerSpec
- * @property {(string | null)} inherit - name of repository where the external scanner config file is placed
- * @property {(AllowlistEntry[] | null)} allowlist - list over CVE's to dismiss
- */
-
-/**
- * @typedef ScannerConfig
- * @property {string} apiVersion - api version
- * @property {string} kind - type of scanner configuration
- * @property {(ScannerMetadata|null)} metadata - metadata
- * @property {(ScannerSpec|null)} spec - spec
- */
-
-/**
- * @typedef PartialCodeScanningAlertRule
- * @property {string[]} tags
- */
-
-/**
- * @typedef PartialCodeScanningAlert
- * @property {number} number
- * @property {PartialCodeScanningAlertRule} rule
- */
-
-/**
- * @typedef CweTagValues
- * @property {string} comment
- * @property {("false positive"|"won't fix"|"used in tests")} reason
- */
+import { ScannerConfig, PartialCodeScanningAlert, AllowlistEntry, CweTagValues  } from './typedefs'
 
 
 /**
@@ -84,7 +37,7 @@ const fetchExternalConfigContent = async (token, repository, scanner) => {
     const yamlPaths = YAML_EXTENSIONS.map(extension => `.entur/security/${scanner}.${extension}`)
 
     let contentPromises = yamlPaths.map(filePath => octokitClient.rest.repos.getContent({
-        owner: "LillieEntur",
+        owner: "Entur",
         repo: repository,
         path: filePath
     }))
