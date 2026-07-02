980d09b91133f1c20e964f0e795989fbf7db982b
Lillie Rugtveit <126776478+LillieEntur@users.noreply.github.com>
2026-07-02 11:25:40 +0200

    feat: add codeql kotlin support warning (#233)
    * feat: add codeql kotlin support warning
    
    * test: codeql kotlin error handler
    
    * chore: remove extra empty lines
    
    * chore: add documentation, and runner os check
    
    * test: add assert step
    
    * test: fix wrong variable
    
    * test: remove temp assert test
    
    * chore: fix failing actionlint check
    
    * fix: should be able to extract beta versions with regex
    
    * test: fix missing should-fail
    
    * fix: use gha-security @v2 for codeql-ktolin-error-handler
    
    * fix: include actions path in uses for codeql-kotlin-error-handler

---
+++ .github/actions/codeql-kotlin-error-handler/index.js
@@ -0,0 +1,56 @@
+// Github doesn't have a way to get previous log lines of a job.
+// So we take advantage of the logs being stored in the actions-runner, so we can look for the error line from CodeQL autobuild.
+// NOTE: if Github one day, allows you to read logs using Official API, then use that instead.
+const getActionRunnerLog = async (fs, glob) => {
+    try {
+        // Globber is used here to fetch the files, there is usually 2 files generated here.
+        // One for the log of current step, and one for the entire job.
+        const globber = await glob.create("~/actions-runner/cached/*/_diag/blocks/*", {
+            followSymbolicLinks: false
+        });
+
+        const files = await globber.glob();
+
+        return files.map(file => fs.readFileSync(file, 'utf8')).join("\n");
+
+    } catch (error) {
+        throw new Error("Failed to get action runner log", { cause: error })
+    }
+}
+
+module.exports = async ({ core, glob }) => {
+    // This action does not support macOS/Windows
+    if (process.env.RUNNER_OS != "Linux")
+        return;
+
+    const fs = require('fs');
+    
+    const actionRunnerLog = await getActionRunnerLog(fs, glob);
+    const supportErrorMessage = actionRunnerLog
+        .split("\n")
+        .find(line =>
+            /Kotlin version (.*) is too recent. CodeQL currently supports versions below (.*)/.test(line));
+
+    // if no message found related to CodeQL kotlin support, silently quit
+    if (!supportErrorMessage)
+        return;
+
+    const matches = supportErrorMessage.match(/Kotlin version (.*) is too recent. CodeQL currently supports versions below (.*)/)
+
+    const [_, kotlinVersion, incompatibleKotlinVersion] = matches;
+
+
+    await core.summary
+        .addHeading(`CodeQL analysis failed with too recent Kotlin version`)
+        .addRaw(`Kotlin ${kotlinVersion} version too recent for CodeQL, CodeQL supports versions below ${incompatibleKotlinVersion}.`)
+        .addBreak()
+        .addRaw("To continue using Code Scan, please wait with Kotlin upgrade until it's supported by CodeQL.")
+        .addBreak()
+        .addBreak()
+        .addRaw('See <a href="https://Github.com/Github/codeql/issues?q=is%3Aissue%20Support%20Kotlin">Github issues</a> for status on CodeQL Kotlin Support')
+        .addSeparator()
+        .write()
+
+
+    core.setFailed(`Kotlin ${kotlinVersion} version too recent for CodeQL, CodeQL supports versions below ${incompatibleKotlinVersion}`)
+}
